9cc34fab0908cf0590e054127fc81832a073d0fb
dexter3679 <dominik.szczesniak.ciech@gmail.com>
2025-11-30 14:07:19 +0100

    Dodanie Komentarzy

--- AnimatedObject.h
+++ AnimatedObject.h
@@ -1,12 +1,31 @@
+/**
+* @file AnimatedObject.h
+* @brief Interfejs dla obiektów animowanych.
+*
+* AnimatedObject dziedziczy po UpdatableObject, co pozwala na integracjê
+* z g³ówn¹ pêtl¹ silnika i aktualizacjê stanu animacji w czasie.
+*/
 #pragma once
 #include "UpdatableObject.h"
-
-// Dziedziczy po UpdatableObject, aby wpasowac sie w glowna petle silnika.
+/**
+* @class AnimatedObject
+* @brief Klasa bazowa (interfejs) dla obiektów posiadaj¹cych animacjê.
+*
+*/
 class AnimatedObject : public virtual UpdatableObject {
 public:
+    /**
+   * @brief Wirtualny destruktor domyœlny.
+   */
     virtual ~AnimatedObject() = default;
 
-    
-    // Przekazujemy dt (delta time), aby animacja byla plynna i niezalezna od FPS.
+    /**
+  * @brief Wykonuje krok animacji.
+  *
+  * Metoda czysto wirtualna. Przekazywany czas (dt) zapewnia p³ynnoœæ
+  * animacji niezale¿n¹ od liczby klatek na sekundê (FPS).
+  *
+  * @param dt Czas, który up³yn¹³ od ostatniej klatki (delta time).
+  */
     virtual void animate(float dt) = 0;
 };
--- SpriteObject.h
+++ SpriteObject.h
@@ -1,26 +1,82 @@
+/**
+* @file SpriteObject.h
+* @brief Klasa ³¹cz¹ca mo¿liwoœæ rysowania bitmap z logik¹ animacji.
+*
+* SpriteObject dziedziczy po BitmapObject oraz AnimatedObject, co pozwala na
+* wyœwietlanie sekwencji grafik (animacji) w okreœlonym czasie.
+*/
 #pragma once
 
 #include "BitmapObject.h"
 #include "AnimatedObject.h"
 #include <map>
+/**
+* @class SpriteObject
+* @brief Klasa reprezentuj¹ca animowany obiekt typu Sprite.
+*
+*/
 
-// L¹czy mozliwosc rysowania bitmap (BitmapObject) z logika animacji (AnimatedObject).
 class SpriteObject : public BitmapObject, public AnimatedObject {
 protected:
-
+    /**
+   * @brief Mapa przechowywuj¹ca definicje animacji.
+   *
+   * Kluczem jest nazwa animacji, a wartoœci¹ wektor identyfikatorów bitmap.
+   */
     std::map<std::string, std::vector<std::string>> m_animations;
+    /**
+    * @brief Nazwa aktualnie odtwarzanej animacji.
+    */
     std::string m_currentAnimation;
-
+    /**
+   * @brief Prêdkoœæ odtwarzania animacji.
+   */
     float m_animSpeed; 
+    /**
+   * @brief Licznik czasu do kontroli zmiany klatek animacji.
+   */
     float m_animTimer; 
 
 public:
-    // Konstruktor przyjmuje tylko pozycje i wymiary. Bitmapy dodaje siê pozniej.
+    /**
+* @brief Konstruktor obiektu Sprite.
+*
+* Inicjalizuje pozycjê, wymiary oraz prêdkoœæ animacji. Bitmapy dodaje siê póŸniej.
+*
+* @param x Pozycja X.
+* @param y Pozycja Y.
+* @param w Szerokoœæ.
+* @param h Wysokoœæ.
+* @param animSpeed Prêdkoœæ animacji (domyœlnie 10.0f).
+*/
     SpriteObject(float x, float y, float w, float h, float animSpeed = 10.0f);
-
+    /**
+ * @brief Dodaje now¹ definicjê animacji.
+ *
+ * @param name Unikalna nazwa animacji.
+ * @param bitmapIDs Wektor zawieraj¹cy identyfikatory bitmap (klatek) dla tej animacji.
+ */
    
     void addAnimation(const std::string& name, const std::vector<std::string>& bitmapIDs);
+    /**
+   * @brief Ustawia aktualnie odtwarzan¹ animacjê.
+   *
+   * @param name Nazwa animacji do ustawienia.
+   */
+
     void setAnimation(const std::string& name);
+    /**
+   * @brief Aktualizuje stan obiektu.
+   *
+   * @param dt Czas, który up³yn¹³ od ostatniej klatki (delta time).
+   */
     void update(float dt) override;
+    /**
+ * @brief Przetwarza logikê animacji.
+ *
+ * Odpowiada za zmianê klatek w zale¿noœci od up³ywu czasu.
+ *
+ * @param dt Czas, który up³yn¹³ od ostatniej klatki (delta time).
+ */
     void animate(float dt) override;
 };
